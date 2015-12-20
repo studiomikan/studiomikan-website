b3d9ffe2e51592c477d4b7ef244f6505a8e1dc9e
okayu <studiomikan@gmail.com>
2015-12-20 15:36:18 +0900

    aboutページ作成中、jQueryのバージョンを2.1.4に変更、ロゴ画像のサイズを2倍に変更

--- src/js/common.js
+++ src/js/common.js
@@ -4,13 +4,14 @@ $(function() {
 	// PJAX設定
 	(function() {
 		$.pjax({
-			area: '#center-block',
+			area: '#global-content-wrapper',
 			link: 'a.pjax-link',
 			ajax: { timeout: 10000 },
 			load: { script: false }
 		});
 		$(document).on('pjax:fetch', function() {
 			// pjaxのページ遷移が行われる前
+			alert('pjax!');
 		});
 		$(document).on('pjax:render', function() {
 			// pjaxのページ遷移が完了した後
