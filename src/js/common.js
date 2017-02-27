af4f3235986efd6456556a8c3ba4e0c425f58a5a
okayu <studiomikan@gmail.com>
2017-02-27 23:22:21 +0900

    リニューアル中

--- src/js/common.js
+++ src/js/common.js
@@ -1,48 +1,124 @@
 // common.js
-$(function() {
-	// PJAX設定
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	// start 背景アニメ
 	(function() {
-		$.pjax({
-			area: '#global-content-wrapper',
-			link: 'a.pjax-link',
-			ajax: { timeout: 10000 },
-			load: { script: false }
-		});
-		$(document).on('pjax:fetch', function() {
-			// pjaxのページ遷移が行われる前
-		});
-		$(document).on('pjax:render', function() {
-			// pjaxのページ遷移が完了した後
-		});
-	})();
-	// スクロール時にメニューに影をつける
-	/*
-	(function() {
-		var onScroll = function() {
-			var head = $('header');
-			if ($(window).scrollTop() > 0) {
-				if (!head.hasClass('border-on')) head.addClass('border-on');
-			} else {
-				if (head.hasClass('border-on')) head.removeClass('border-on');
+		// 設定
+		var color = 'rgba(255, 170, 1, 0.3)';
+		var maxSize = 30;
+		var minSize = 100;
+		var circleNum = 15;
+		var minSpeed = 0.1;
+		var maxSpeed = 2;
+		var bound = 0.2;
+
+		// フィールド
+		var canvas = null;
+		var context = null;
+		var circles = [];
+
+		// 初期化
+		var init = function() {
+			canvas = document.getElementById('back-anim-canvas');
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+			context = canvas.getContext('2d');
+		};
+
+		// サークル情報を生成
+		var createCircles = function() {
+			for (var i = 0; i < circleNum; i++) {
+				var c = {
+					//x: canvas.width / 2,
+					x: 125,
+					y: canvas.height / 2,
+					size: minSize + (maxSize - minSize) * Math.random(),
+					angle: Math.PI * 2 * Math.random(),
+					speed: minSpeed + (maxSpeed - minSpeed) * Math.random()
+				};
+				circles.push(c);
 			}
-		}
-		$(window).on('scroll', onScroll);
-		onScroll();
+		};
+
+		// リサイズ時の動作の初期化
+		var initOnResize = function() {
+			var timer = null;
+			window.addEventListener('resize', function() {
+				if (timer === null) {
+					window.clearTimeout(timer);
+				}
+				timer = window.setTimeout(function() {
+					canvas.width = window.innerWidth;
+					canvas.height = window.innerHeight;
+					console.log('resize: canvas ' + canvas.width + ',' + canvas.height);
+					console.log('resize: canvas ' + window.innerWidth + ',' + window.innerHeight);
+					timer = null;
+				}, 200);
+			});
+		};
+
+		// 描画する
+		var draw = function() {
+			// クリア
+			context.beginPath();
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.closePath();
+			for (var i = 0; i < circles.length; i++) {
+				var c = circles[i];
+				context.beginPath();
+				context.fillStyle = color;
+				context.arc(c.x, c.y, c.size, 0, Math.PI*2, false);
+				context.fill();
+				context.closePath();
+			}
+		};
+
+		// サークルを移動させる
+		var move = function() {
+			var canvasWidth = canvas.width;
+			var canvasHeight = canvas.height;
+			for (var i = 0; i < circles.length; i++) {
+				var c = circles[i];
+				// 移動
+				var moveX = c.speed * Math.cos(c.angle);
+				var moveY = c.speed * Math.sin(c.angle);
+				c.x += moveX;
+				c.y += moveY;
+				// 跳ね返り
+				if (c.x <= 0 || canvasWidth <= c.x) {
+					c.angle = Math.atan2(moveY, -moveX);
+					c.x = c.x <= 0 ? 0 : canvasWidth;
+				}
+				if (c.y <= 0 || canvasHeight <= c.y) {
+					c.angle = Math.atan2(-moveY, moveX);
+					c.y = c.y <= 0 ? 0 : canvasHeight;
+				}
+			}
+		};
+
+		// タイマー開始
+		var startTimer = function() {
+			var requestAnimationFrame = 
+				window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+				window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+			var loopFunc = function() {
+				try {
+					move();
+					draw();
+				} catch (e) {
+					console.log(e);
+				}
+				requestAnimationFrame(loopFunc);
+			};
+			requestAnimationFrame(loopFunc);
+		};
+
+		init();
+		createCircles();
+		initOnResize();
+		startTimer();
 	})();
-	*/
-	// メニューボタン
-	(function() {
-		var button = $('header button.menu');
-		var target = $(button.attr('target'));
-		var win = $(window);
-		button.on('click', function() {
-			target.animate({
-				height: 'toggle'
-			}, 200);
-		});
-		win.on('resize', function() {
-			if (win.width() > 800) target.show();
-		});
-	})();
+	// end 背景アニメ
 });
 
